e191a9b2d75f89b605af00f9f7412c80a683e3f6
Christopher Zenzel <christopher@arendellekingdom.com>
2024-02-17 13:21:37 -0500

    Include exceptions

--- project4/tests/SummaryTest.php
+++ project4/tests/SummaryTest.php
@@ -2,38 +2,39 @@
 
 namespace Project4\Arrays;
 
+use Project4\Exceptions;
 use PHPUnit\Framework\TestCase;
 
 final class SummaryTest extends TestCase
 {
     public function testClassConstructor()
     {
-        $summary = new Project4\Arrays\Summary();
+        $summary = new Summary();
         $this->assertInstanceOf(Summary::class, $summary);
     }
 
     public function testSum()
     {
-        $summary = new Project4\Arrays\Summary();
+        $summary = new Summary();
         $this->assertEquals(15, $summary->sum([1, 2, 3, 4, 5]));
     }
 
     public function testCheck()
     {
-        $summary = new Project4\Arrays\Summary();
+        $summary = new Summary();
         $this->expectException(NumericException::class);
         $summary->check([1, 2, 'a', 4, 5]);
     }
 
     public function testCheckWithNoException()
     {
-        $summary = new Project4\Arrays\Summary();
+        $summary = new Summary();
         $this->assertNull($summary->check([1, 2, 3, 4, 5]));
     }
 
     public function testCheckWithEmptyArray()
     {
-        $summary = new Project4\Arrays\Summary();
+        $summary = new Summary();
         $this->assertNull($summary->check([]));
     }
 }
