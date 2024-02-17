45a696a0700c40f1b39ad087c3f452bde3d7357b
Christopher Zenzel <christopher@arendellekingdom.com>
2024-02-17 13:17:52 -0500

    Work on Project 4 unit testing

---
+++ project4/src/Project4/Arrays/SummaryTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Project4\Arrays;
+
+use PHPUnit\Framework\TestCase;
+
+final class SummaryTest extends TestCase
+{
+    public function testClassConstructor()
+    {
+        $summary = new Summary();
+        $this->assertInstanceOf(Summary::class, $summary);
+    }
+
+    public function testSum()
+    {
+        $summary = new Summary();
+        $this->assertEquals(15, $summary->sum([1, 2, 3, 4, 5]));
+    }
+
+    public function testCheck()
+    {
+        $summary = new Summary();
+        $this->expectException(NumericException::class);
+        $summary->check([1, 2, 'a', 4, 5]);
+    }
+
+    public function testCheckWithNoException()
+    {
+        $summary = new Summary();
+        $this->assertNull($summary->check([1, 2, 3, 4, 5]));
+    }
+
+    public function testCheckWithEmptyArray()
+    {
+        $summary = new Summary();
+        $this->assertNull($summary->check([]));
+    }
+}
