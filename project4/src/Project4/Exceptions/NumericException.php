93521c858cda126a3c1a45879a28ca3dd8fcdfe4
Christopher Zenzel <christopher@arendellekingdom.com>
2024-02-17 13:26:10 -0500

    Fix numeric exception

--- project4/src/Project4/Exceptions/NumericException.php
+++ project4/src/Project4/Exceptions/NumericException.php
@@ -2,9 +2,9 @@
 
 namespace Project4\Exceptions;
 
-class NumericException extends Exception
+class NumericException extends \Exception
 {
-    public function __construct($message = 'Numeric array expected', $code = 0, Exception $previous = null)
+    public function __construct($message = 'Numeric array expected', $code = 0, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
