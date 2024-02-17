7ea4a34413a0cde6193de1028b7fd84819871834
Christopher Zenzel <christopher@arendellekingdom.com>
2024-02-17 13:20:14 -0500

    Fix unit testing

--- project4/tests/SummaryTest.php
+++ project4/tests/SummaryTest.php
@@ -8,32 +8,32 @@ final class SummaryTest extends TestCase
 {
     public function testClassConstructor()
     {
-        $summary = new Summary();
+        $summary = new Project4\Arrays\Summary();
         $this->assertInstanceOf(Summary::class, $summary);
     }
 
     public function testSum()
     {
-        $summary = new Summary();
+        $summary = new Project4\Arrays\Summary();
         $this->assertEquals(15, $summary->sum([1, 2, 3, 4, 5]));
     }
 
     public function testCheck()
     {
-        $summary = new Summary();
+        $summary = new Project4\Arrays\Summary();
         $this->expectException(NumericException::class);
         $summary->check([1, 2, 'a', 4, 5]);
     }
 
     public function testCheckWithNoException()
     {
-        $summary = new Summary();
+        $summary = new Project4\Arrays\Summary();
         $this->assertNull($summary->check([1, 2, 3, 4, 5]));
     }
 
     public function testCheckWithEmptyArray()
     {
-        $summary = new Summary();
+        $summary = new Project4\Arrays\Summary();
         $this->assertNull($summary->check([]));
     }
 }
