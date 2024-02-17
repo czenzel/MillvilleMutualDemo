83ca25b0d3fa568e084d2b45554f3fcb4d7f0d5b
Christopher Zenzel <christopher@arendellekingdom.com>
2024-02-17 13:34:34 -0500

    Fix exception handling

--- project4/src/Project4/Arrays/Summary.php
+++ project4/src/Project4/Arrays/Summary.php
@@ -14,7 +14,7 @@ class Summary
         {
             if (!is_numeric($value))
             {
-                throw new NumericException();
+                throw new \Project4\Exceptions\NumericException();
             }
         }
     }
