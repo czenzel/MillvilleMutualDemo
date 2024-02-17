9a6a551bb8b8197a87a63f9e36a806a67aea697c
Christopher Zenzel <christopher@arendellekingdom.com>
2024-02-16 23:45:52 -0500

    Work on the projects

--- project4/index.php
+++ project4/index.php
@@ -1,21 +1,32 @@
-<?php
-require_once(dirname(__FILE__) . '/vendor/autoload.php');
-?>
+<?php require_once(dirname(__FILE__) . '/vendor/autoload.php'); ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Project 4</title>
-</head>
-<body>
-    <h1>Project 4</h1>
-    <p>
+    <head>
+        <title>Project 2</title>
+    </head>
+    <body>
         <?php
-        $demo = new \Project4\Demo\Demonstration();
+        $navigation = new Project4\Bootstrap\Navigation();
+        $navigation->render();
         ?>
-        <h2>Summary of Integers: <?= $demo->run(); ?></h2>
-        <p>
-            <pre><?= $demo->values_as_json(); ?></pre>
-        </p>
-    </p>
-</body>
+        <main>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p>
+                            <?php
+                            $demo = new \Project4\Demo\Demonstration();
+                            ?>
+                            <h2>Summary of Integers: <?= $demo->run(); ?></h2>
+                            <p>
+                                <pre><?= $demo->values_as_json(); ?></pre>
+                            </p>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </body>
 </html>
---
+++ project4/src/Project4/Bootstrap/Navigation.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Project4\Bootstrap;
+
+class Navigation 
+{
+    public function render()
+    {
+        $navbar = <<<EOL
+        <div class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Project 4</a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+              <a class="nav-link" href="/">Home</a>
+              </li>
+            </ul>
+            </div>
+        </div>
+        EOL;
+
+        echo $navbar;
+    }
+}
